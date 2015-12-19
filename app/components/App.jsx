4516d1be6816b24064dc6296f206f25db11b1ef0
Philip Cavaco <philipcavaco@gmail.com>
2015-12-19 17:24:49 +0000

    Fix babel configf file name and get Names working

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,46 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 
-export default () => {
-  return <Note />;
-};
+export default class App extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {
+  		notes: [
+		  {
+		    id: uuid.v4(),
+		    task: 'Learn Webpack'
+		  },
+		  {
+		    id: uuid.v4(),
+		    task: 'Learn React'
+		  },
+		  {
+		    id: uuid.v4(),
+		    task: 'Do laundry'
+		  }
+		]
+  	};
+  }
+
+  render() {
+  	const notes = this.state.notes;
+    return (
+      <div>
+      	<button className="add-note" onClick={this.addNote}>+
+      	</button>
+        <Notes items={notes} />
+      </div>
+    );
+  }
+
+  addNote = () => {
+  	this.setState({
+  		notes: this.state.notes.concat([{
+  			id: uuid.v4(),
+  			task: 'New task'
+  		}])
+  	});
+  }
+}
