848bb9054f824e8ed37b8ffd9315c7b336e841f5
Philip Cavaco <philipcavaco@gmail.com>
2015-12-20 17:11:07 +0000

    Integrate Alt Flux library with app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,46 +1,48 @@
-import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
   	super(props);
 
-  	this.state = {
-  		notes: [
-		  {
-		    id: uuid.v4(),
-		    task: 'Learn Webpack'
-		  },
-		  {
-		    id: uuid.v4(),
-		    task: 'Learn React'
-		  },
-		  {
-		    id: uuid.v4(),
-		    task: 'Do laundry'
-		  }
-		]
-  	};
+  	this.state = NoteStore.getState();
+  }
+
+  componentDidMount() {
+  	NoteStore.listen(this.storeChanged);
+  }
+
+  componentWillUnmount() {
+  	NoteStore.unlisten(this.storeChanged);
+  }
+
+  storeChanged = (state) => {
+  	this.setState(state);
   }
 
   render() {
   	const notes = this.state.notes;
+
     return (
       <div>
       	<button className="add-note" onClick={this.addNote}>+
       	</button>
-        <Notes items={notes} />
+        <Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
       </div>
     );
   }
 
   addNote = () => {
-  	this.setState({
-  		notes: this.state.notes.concat([{
-  			id: uuid.v4(),
-  			task: 'New task'
-  		}])
-  	});
+  	NoteActions.create({task: 'New task'});
+  }
+
+  editNote = (noteId, task) => {
+  	NoteActions.update({noteId, task});
+  }
+
+  deleteNote = (noteId) => {
+  	NoteActions.delete(id);
   }
 }
